b43c3453cde22c3baa9bf3f92930aaf9b3ab34a1
ga111o <ga111o@proton.me>
2024-06-16 13:13:24 +0900

    ✨ cpu, memory, gpu, vram usage

--- main.py
+++ main.py
@@ -10,14 +10,11 @@ def index():
 
 @app.route('/usage')
 def system_usage():
-    # CPU 사용량
     cpu_usage = psutil.cpu_percent(interval=1)
 
-    # RAM 사용량
     memory_info = psutil.virtual_memory()
     ram_usage = memory_info.percent
 
-    # GPU 사용량
     gpus = GPUtil.getGPUs()
     if gpus:
         gpu = gpus[0]
